1730b6eb3427094f65a541c83c07cebcc0f34e2c
Hun Namkoong <hnamkoong1@gmail.com>
2016-01-13 22:30:00 +0900

    Initial Commit

---
+++ Tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorial
+//
+//  Created by Hun Namkoong on 2016. 1. 13..
+//  Copyright © 2016년 Hun Namkoong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
